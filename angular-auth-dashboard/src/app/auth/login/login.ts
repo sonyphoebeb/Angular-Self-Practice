efbf2760873c68cc9ae869bee11e26ed3c524437
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-10-25 00:15:09 +0530

    auth dashboard project completed

--- angular-auth-dashboard/src/app/auth/login/login.ts
+++ angular-auth-dashboard/src/app/auth/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from 'express';
+import { AuthService } from '../auth.service.ts';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  username = '';
+  password = '';
+  error = '';
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  login() {
+    if (this.auth.login(this.username, this.password)) {
+      // Using Angular's Router, not Express Router
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.error = 'Invalid credentials!';
+    }
+  }
 }
--- angular-auth-dashboard/src/app/dashboard/dashboard/dashboard.ts
+++ angular-auth-dashboard/src/app/dashboard/dashboard/dashboard.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service.ts';
+import { Router } from 'express';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  constructor(private auth: AuthService, private router: Router) {}
 
+  logout() {
+    this.auth.logout();
+    // Using Angular's Router, not Express Router
+    this.router.navigate(['/auth/login']);
+  }
 }
