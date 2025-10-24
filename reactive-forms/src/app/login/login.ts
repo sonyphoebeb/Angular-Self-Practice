16cd6d7aeb9bdd89df37b4aea7875bc39dc88cc5
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-10-24 23:37:16 +0530

    login ts file updated

--- reactive-forms/src/app/login/login.ts
+++ reactive-forms/src/app/login/login.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class Login {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login Data:', this.loginForm.value);
+      alert('✅ Login successful!');
+    } else {
+      alert('⚠️ Please fill in valid details.');
+    }
+  }
 }
