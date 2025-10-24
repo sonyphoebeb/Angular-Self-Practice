3cad07975b0a4a57f42cf2b2b94b56a5a90f38e7
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-10-25 00:16:20 +0530

    added auth guard to app.routes.ts

--- angular-auth-dashboard/src/app/app.routes.ts
+++ angular-auth-dashboard/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Dashboard } from './dashboard/dashboard/dashboard';
+import { authGuard } from './auth/auth.guard.ts';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: 'auth/login', component: Login },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+];
